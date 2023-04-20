337f300f13aefa8a99907b63b3194371c01a54aa
carterweldon <cawe4a6@student.oaisd.org>
2023-04-20 13:11:59 -0400

    asdaaassadw
    dASaaasd

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+        public void LoadGameOver()
+        {
+            SceneManager.LoadScene("EndScreen");
+        }
+
+        public void QuitGame()
+        {
+        Debug.Log("Quitting Game...");
+            Application.Quit();
+        }
+    }
+
+
